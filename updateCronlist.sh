8c4b69c1cd624c3090555f4e89f4aa2181d7f2b0
openwrt <jddocker@openwrt>
2021-09-30 20:30:24 +0800

    优化生成cron

--- updateCronlist.sh
+++ updateCronlist.sh
@@ -8,14 +8,7 @@ FileCronlist=$WorkDir/docker/crontab_list.test
 
 ## crontab_list 头部内容
 
-echo 'SHELL=/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
-50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
-# 收集助力码
-30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
-# 配置更新
-17 3 * * * sh /scripts/docker/default_task.sh >> /scripts/logs/default_task.log 2>&1' >$FileCronlist
+echo -e "SHELL=/bin/bash \nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \n# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重) \n50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf \n# 收集助力码 \n30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1 \n# 配置更新 \n17 3 * * * sh /scripts/docker/default_task.sh >> /scripts/logs/default_task.log 2>&1" >$FileCronlist
 echo -e "############################################\n" >>$FileCronlist
 
 ## 执行写入
@@ -27,8 +20,11 @@ do
     if [[ -n $TaskName ]]; then
         echo -e "# $TaskName" >> $FileCronlist
         cronexp=$(grep -E "tag.+enabled" $file |cut -d' ' -f1-5)
-        if [[ ! -z $cronexp ]]; then
+        cronexp1=$(grep -oP 'cron (((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ){5}' $file |cut -d' ' -f2-)
+        if [[ -n $cronexp ]]; then
             echo "$cronexp node /scripts/$file >> /scripts/logs/${file%%.*}.log 2>&1" >> $FileCronlist
+        elif [[ -n $cronexp1 ]]; then
+            echo "$cronexp1 node /scripts/$file >> /scripts/logs/${file%%.*}.log 2>&1" >> $FileCronlist
         else
             echo "0 9 * * * node /scripts/$file >> /scripts/logs/${file%%.*}.log 2>&1" >> $FileCronlist
         fi
